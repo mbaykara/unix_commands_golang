e865f0201f0e38794481b2bf4e49c0d61cd9f472
Mehmet Ali Baykara <Baykara@encoway.de>
2020-11-15 01:52:21 +0100

    1. part

--- src/cat.go
+++ src/cat.go
@@ -8,23 +8,37 @@ import(
 	
 
 )
+//TODOS
+// open and read file
+// concatenate the file
 
 func main() {
 
-	s := os.Args[1]
-	f, err := os.Open(s)
-	if err != nil{
+	if len(os.Args) < 2 {
+		os.Exit(0)
+	}else{
+		read_file(os.Args[1])
+	}
+	
+ 
+	
+}
+
+func read_file(f string){
+	file, err := os.Open(f)
+	if err != nil {
 		panic(err)
 	}
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
 	for scanner.Scan(){
 		fmt.Println(scanner.Text())
 	}
 	if err :=scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
 }
